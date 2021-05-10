f2eb070087a78edfb6e273f81f9703c33abd4f8d
ed <edchapman88@gmail.com>
2021-05-10 17:40:09 +0100

    Update version number, license and setup.py with the up-to-date information.

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 import setuptools
 
 from os import path
+
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(
     name="side-kick",
-    version="0.0.1",
-    author="Ben Currie",
-    author_email="benjamin.w.currie@gmail.com",
+    version="0.1.14",
+    author="Natoora Technology",
+    author_email="technology@natoora.com",
     description="A lightweight task runner for django with easy admin controls",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url="https://github.com/Natoora/side-kick",
-    license="MIT License",
+    url="https://github.com/Natoora/sidekick",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
