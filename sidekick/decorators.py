dfdfc2d2dcb952be59e03ab5a213fcae3ca2cda8
ed <edchapman88@gmail.com>
2022-02-01 13:29:43 +0000

    Move db connection check into try/except so it doesn't throw errors in automated tests.

--- sidekick/decorators.py
+++ sidekick/decorators.py
@@ -1,31 +1,26 @@
 import inspect
+import logging
 
 from django.db import connection
 
+logger = logging.getLogger(__name__)
 
-# This is here in case you have tasks decorated before running migrations
-# It will just return the original function if the table does not exist
+try:
+    # This is here in case you have tasks decorated before running migrations
+    # It will just return the original function if the table does not exist
+    db_ready = 'sidekick_registeredtask' in connection.introspection.table_names()
+except Exception as e:
+    logger.info(f"DB not ready when registering SideKick task: {e}")
+    db_ready = False
 
-
-def db_connected():
-    from django.db import connection
-    try:
-        db_conn = True
-        connection.ensure_connection()
-    except Exception as e:
-        db_conn = False
-    return db_conn
-
-
-if db_connected() and 'sidekick_registeredtask' in connection.introspection.table_names():
-
+if db_ready:
     def sidekick_task(fn):
         """
-        Strip back any unnecessary text so that you can grab the task name and the app
-        it came from in order to create a RegisteredTask instance in the correct format
-        :param fn: Any function you want to run as a task
-        :return: The original function
-        """
+            Strip back any unnecessary text so that you can grab the task name and the app
+            it came from in order to create a RegisteredTask instance in the correct format
+            :param fn: Any function you want to run as a task
+            :return: The original function
+            """
         from sidekick.models import RegisteredTask
 
         src = inspect.getsource(fn)
@@ -38,7 +33,6 @@ if db_connected() and 'sidekick_registeredtask' in connection.introspection.tabl
             st.task_name = task_string
             st.save()
         return fn
-
 else:
     def sidekick_task(fn):
         return fn
