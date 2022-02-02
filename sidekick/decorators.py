fa77db2986d11b2931646851b91c62486b4ec5b2
ed <edchapman88@gmail.com>
2022-02-02 16:39:59 +0000

    Wrap decorator function logic in try/except so that it doesn't throw errors when the db is not available. (i.e. What used to be:   `if 'sidekick_registeredtask' in connection.introspection.table_names():`)

--- sidekick/decorators.py
+++ sidekick/decorators.py
@@ -1,20 +1,19 @@
 import inspect
-from django.db import connection
+import logging
 
-# This is here in case you have tasks decorated before running migrations
-# It will just return the original function if the table does not exist
+logger = logging.getLogger(__name__)
 
-if 'sidekick_registeredtask' in connection.introspection.table_names():
 
-    def sidekick_task(fn):
-        """
-        Strip back any unnecessary text so that you can grab the task name and the app
-        it came from in order to create a RegisteredTask instance in the correct format
-        :param fn: Any function you want to run as a task
-        :return: The original function
-        """
-        from sidekick.models import RegisteredTask
+def sidekick_task(fn):
+    """
+    Strip back any unnecessary text so that you can grab the task name and the app
+    it came from in order to create a RegisteredTask instance in the correct format
 
+    :param: fn, Any function you want to run as a task
+    :return: The original function
+    """
+    try:
+        from .models import RegisteredTask
         src = inspect.getsource(fn)
         module_name = inspect.getmodule(fn).__name__.split('.')[0]
         task_string = src.split('(')[0][17:].replace('f ', '{} --'.format(module_name))
@@ -25,7 +24,6 @@ if 'sidekick_registeredtask' in connection.introspection.table_names():
             st.task_name = task_string
             st.save()
         return fn
-
-else:
-    def sidekick_task(fn):
+    except Exception as e:
+        logger.info(f"Problem registering sidekick_task decorator: {e}")
         return fn
